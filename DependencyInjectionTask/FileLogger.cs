63b4c5b7434676046346e251a89ba6b63acd0fb7
ariefriski <arief.riski11@gmail.com>
2024-02-28 16:28:56 +0700

    Dependency Injection

---
+++ DependencyInjectionTask/FileLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionTask
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _logFilePath;
+
+        public FileLogger(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+
+        public void Log(string message)
+        {
+            using (var writer = new StreamWriter(_logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now} - {message}");
+            }
+        }
+    }
+}
---
+++ DependencyInjectionTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjectionTask
+{
+    public class Program
+    {
+
+        private readonly ILogger _logger;
+
+        public Program(ILogger logger)
+        {
+            _logger = logger;
+        }
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                 .AddSingleton<ILogger, ConsoleLogger>() // Registrasi ILogger dan implementasinya di sini
+                 .BuildServiceProvider();
+
+            // Ambil instance logger dari service provider
+            var logger = serviceProvider.GetService<ILogger>();
+
+            // Pastikan logger tidak null sebelum membuat instance Program
+            if (logger != null)
+            {
+                // Buat instance program
+                var program = new Program(logger);
+
+                // Jalankan program
+                program.Run();
+            }
+            else
+            {
+                Console.WriteLine("Failed to resolve ILogger from the service provider.");
+            }
+
+            Console.ReadLine();
+        }
+
+        public void Run()
+        {
+            _logger.Log("Hello, World!");
+        }
+    }
+}
